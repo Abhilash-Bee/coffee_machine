530535f9d89a752218b65e21059c9f78c5e50672
Abhilash <abhilashjanarrdhan@gmail.com>
2022-08-24 09:25:09 +0530

    Coffee Machine Project

---
+++ main.py
@@ -0,0 +1,23 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+options = menu.get_items()
+
+while is_on:
+    user_input = input(f"What would you like to have ({options}) :: ").casefold()
+    if user_input == "off":
+        exit(0)
+    elif user_input == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(user_input)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
